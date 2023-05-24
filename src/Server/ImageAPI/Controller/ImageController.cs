0eef9ba720d9407aa4420730574cef5f991c2275
Jackpieking <ledinhdangkhoa10a9@gmail.com>
2023-05-24 09:35:56 +0700

    initial frontend codebase

---
+++ src/Server/ImageAPI/Controller/ImageController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
+
+namespace ImageAPI.Controller;
+
+[Produces(contentType: MediaTypeNames.Application.Json)]
+[Consumes(contentType: MediaTypeNames.Application.Json)]
+[Route(template: "api/[controller]")]
+[ApiController]
+
+public class ImageController : ControllerBase
+{
+	[HttpGet]
+	public IActionResult GetImageFromOneDriveStorage() => Ok(value: string.Empty);
+}
