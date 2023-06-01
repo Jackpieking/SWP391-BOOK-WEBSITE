a89ce02be76951b2ce847a5540f3a3a127d409ee
Jackpieking <ledinhdangkhoa10a9@gmail.com>
2023-06-01 07:49:03 +0700

    modified UserAccessDataSeeding.cs

--- src/Server/MangaManagementAPI/Data/ModelDataSeedings/UserAccessDataSeeding.cs
+++ src/Server/MangaManagementAPI/Data/ModelDataSeedings/UserAccessDataSeeding.cs
@@ -13,10 +13,13 @@ internal class UserAccessDataSeeding : IEntityTypeConfiguration<UserInfo>
 		UserInfo userAccess = new()
 		{
 			ID = 1,
+			UserName = "jackpieking",
+			Password = "Jackpie2003",
+			Role = Role.ADMIN,
 			FullName = "Le Dinh Dang Khoa",
 			Age = 20,
 			Gender = Gender.MALE,
-			BirthDay = new(2003, 12, 03),
+			BirthDay = new(year: 2003, month: 12, day: 03),
 			Email = "ledinhdangkhoa10a9@gmail.com",
 			AccountBalance = 10_000_000,
 			UserIdentifier = Guid.Parse(input: "585a96f6-3d1b-4fa3-83f1-e2da38ecd92b")
