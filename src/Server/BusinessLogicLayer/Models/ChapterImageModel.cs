8197f827fdee7fca1636333e139e5aecf3b59add
Jackpieking <ledinhdangkhoa10a9@gmail.com>
2023-06-04 17:23:48 +0700

    Add implementation for all models

---
+++ src/Server/BusinessLogicLayer/Models/ChapterImageModel.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace MangaManagementAPI.Models;
+
+public class ChapterImageModel
+{
+	public Guid ImageIdentifier { get; set; }
+
+	public short ImageNumber { get; set; }
+
+	public string ImageURL { get; set; }
+
+	public Guid ChapterIdentifier { get; set; }
+
+	public ChapterModel Chapter { get; set; }
+}
