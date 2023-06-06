2d308301680a54faf10b47c9bb06900d58dbd741
Jackpieking <ledinhdangkhoa10a9@gmail.com>
2023-06-07 01:19:33 +0700

    Delete default bootstrap lib on client project

--- src/NewClient/Pages/Index.cshtml.cs
+++ src/NewClient/Pages/Index.cshtml.cs
@@ -1,20 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Net.Http;
 
-namespace NewClient.Pages
+namespace NewClient.Pages;
+
+public class IndexModel : PageModel
 {
-	public class IndexModel : PageModel
-	{
-		private readonly ILogger<IndexModel> _logger;
+    private readonly ILogger<IndexModel> _logger;
+    private readonly IHttpClientFactory _httpClientFactory;
+    private const string MANGA_API = "MangaAPI";
 
-		public IndexModel(ILogger<IndexModel> logger)
-		{
-			_logger = logger;
-		}
+    public IndexModel(
+        ILogger<IndexModel> logger,
+        IHttpClientFactory httpClientFactory)
+    {
+        _logger = logger;
+        _httpClientFactory = httpClientFactory;
+    }
 
-		public void OnGet()
-		{
+    public void OnGet()
+    {
+        _logger.LogInformation(message: $"[{DateTime.Now}]: Start -> HTTP Request GET On Index page");
 
-		}
-	}
+        var httpClient = _httpClientFactory.CreateClient(name: MANGA_API);
+
+
+
+        _logger.LogInformation(message: $"[{DateTime.Now}]: End -> HTTP Request GET On Index page");
+    }
 }
--- src/NewClient/Program.cs
+++ src/NewClient/Program.cs
@@ -1,24 +1,36 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
 
-// Add services to the container.
-builder.Services.AddRazorPages();
+var builder = WebApplication.CreateBuilder(args: args);
+
+var services = builder.Services;
+
+services
+    .AddHttpClient(
+        name: "MangaAPI",
+        configureClient: httpClient =>
+    {
+        httpClient.BaseAddress = new(uriString: "https://localhost:7174");
+        httpClient.Timeout = TimeSpan.FromSeconds(value: 5);
+    });
+
+services.AddRazorPages();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
-	app.UseExceptionHandler("/Error");
-	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-	app.UseHsts();
+    app.UseDeveloperExceptionPage();
 }
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
+app
+    .UseExceptionHandler(errorHandlingPath: "/Error")
+    .UseHsts()
+    .UseHttpsRedirection()
+    .UseStaticFiles()
+    .UseRouting();
 
 app.MapRazorPages();
 
