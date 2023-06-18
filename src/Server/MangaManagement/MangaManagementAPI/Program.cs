abf877250bdb65763b9a8c008ae57ea842d74bfc
Wolrine <volam030102@gmail.com>
2023-06-19 01:23:19 +0700

    Add admin manager razer page

--- src/Server/MangaManagement/MangaManagementAPI/Program.cs
+++ src/Server/MangaManagement/MangaManagementAPI/Program.cs
@@ -25,95 +25,102 @@ var builder = WebApplication.CreateBuilder(args: args);
 
 //Configure logging
 builder.Host.ConfigureLogging(configureLogging: logging
-	=> logging.AddConsole());
+    => logging.AddConsole());
 
 //add services to application
 var services = builder.Services;
 
 //others
 services
-	.AddScoped<IUnitOfWork, UnitOfWork>()
-	.AddScoped<EntityManagementService>()
-	.AddScoped<TruyenQQPageHandlerService>()
-	.AddScoped<ApiCallingService>()
-	.ConfigureOptions<DatabaseOptionUpdates>();
+    .AddScoped<IUnitOfWork, UnitOfWork>()
+    .AddScoped<EntityManagementService>()
+    .AddScoped<TruyenQQPageHandlerService>()
+    .AddScoped<ApiCallingService>()
+    .ConfigureOptions<DatabaseOptionUpdates>();
 
 //mapper profile
 services
-	.AddAutoMapper(
-				   typeof(UserInfoEntityAndUserInfoModelProfile),
-				   typeof(TransactionHistoryEntityAndTransactionHistoryModelProfile),
-				   typeof(ReviewComicEntityAndReviewComicModelProfile),
-				   typeof(ReviewChapterEntityAndReviewChapterModelProfile),
-				   typeof(ReadingHistoryEntityAndReadingHistoryModelProfile),
-				   typeof(PublisherEntityAndPublisherModelProfile),
-				   typeof(ComicSavingEntityAndComicSavingModelProfile),
-				   typeof(ComicEntityAndComicModelProfile),
-				   typeof(ComicCategoryEntityAndComicCategoryModelProfile),
-				   typeof(ChapterImageEntityAndChapterImageModelProfile),
-				   typeof(ChapterEntityAndChapterModelProfile),
-				   typeof(CategoryEntityAndCategoryModelProfile),
-				   typeof(BuyingHistoryEntityAndBuyingHistoryModelProfile),
-				   typeof(ComicLikeEntityAndComicLikeModelProfile),
-				   typeof(ComicModelToGetAllComicDtoProfile),
-				   typeof(ComicModelToGetComicDetailDtoProfile),
-				   typeof(ChapterImageToGetAllChapterImageOfAChapterDtoProfile),
-				   typeof(UpdateCrawlDataToDatabaseAction_In_DtoToEntityProfile));
+    .AddAutoMapper(
+                   typeof(UserInfoEntityAndUserInfoModelProfile),
+                   typeof(TransactionHistoryEntityAndTransactionHistoryModelProfile),
+                   typeof(ReviewComicEntityAndReviewComicModelProfile),
+                   typeof(ReviewChapterEntityAndReviewChapterModelProfile),
+                   typeof(ReadingHistoryEntityAndReadingHistoryModelProfile),
+                   typeof(PublisherEntityAndPublisherModelProfile),
+                   typeof(ComicSavingEntityAndComicSavingModelProfile),
+                   typeof(ComicEntityAndComicModelProfile),
+                   typeof(ComicCategoryEntityAndComicCategoryModelProfile),
+                   typeof(ChapterImageEntityAndChapterImageModelProfile),
+                   typeof(ChapterEntityAndChapterModelProfile),
+                   typeof(CategoryEntityAndCategoryModelProfile),
+                   typeof(BuyingHistoryEntityAndBuyingHistoryModelProfile),
+                   typeof(ComicLikeEntityAndComicLikeModelProfile),
+                   typeof(ComicModelToGetAllComicDtoProfile),
+                   typeof(ComicModelToGetComicDetailDtoProfile),
+                   typeof(ChapterImageToGetAllChapterImageOfAChapterDtoProfile),
+                   typeof(UpdateCrawlDataToDatabaseAction_In_DtoToEntityProfile));
 
 //cors
 services
-	.AddCors(setupAction: cors => cors.AddDefaultPolicy(configurePolicy: policy =>
-	{
-		policy
-			.AllowAnyHeader()
-			.AllowAnyMethod()
-			.AllowAnyOrigin();
-	}));
+    .AddCors(setupAction: cors => cors.AddDefaultPolicy(configurePolicy: policy =>
+    {
+        policy
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowAnyOrigin();
+    }));
 
 //dbcontext
 services
-	.AddDbContextPool<MangaContext>((service, config) =>
-	{
-		var databaseOptions = service.GetRequiredService<IOptions<DatabaseOptions>>().Value;
+    .AddDbContextPool<MangaContext>((service, config) =>
+    {
+        var databaseOptions = service.GetRequiredService<IOptions<DatabaseOptions>>().Value;
 
-		config
-			.UseNpgsql(connectionString: databaseOptions.DefaultConnectionString, npgsqlOptionsAction: config =>
-			{
-				config
-					.MigrationsAssembly(assemblyName: Assembly.GetExecutingAssembly().FullName)
-					.EnableRetryOnFailure(maxRetryCount: databaseOptions.MaxRetryCount)
-					.CommandTimeout(commandTimeout: databaseOptions.CommandTimeOut);
-			})
-			.EnableDetailedErrors(detailedErrorsEnabled: databaseOptions.EnableDetailedErrors)
-			.EnableServiceProviderCaching(cacheServiceProvider: databaseOptions.EnableServiceProviderCaching)
-			.EnableSensitiveDataLogging(sensitiveDataLoggingEnabled: databaseOptions.EnableSensitiveDataLogging)
-			.EnableThreadSafetyChecks(enableChecks: databaseOptions.EnableThreadSafetyCheck);
-
-
-	});
+        config
+            .UseNpgsql(connectionString: databaseOptions.DefaultConnectionString, npgsqlOptionsAction: config =>
+            {
+                config
+                    .MigrationsAssembly(assemblyName: Assembly.GetExecutingAssembly().FullName)
+                    .EnableRetryOnFailure(maxRetryCount: databaseOptions.MaxRetryCount)
+                    .CommandTimeout(commandTimeout: databaseOptions.CommandTimeOut);
+            })
+            .EnableDetailedErrors(detailedErrorsEnabled: databaseOptions.EnableDetailedErrors)
+            .EnableServiceProviderCaching(cacheServiceProvider: databaseOptions.EnableServiceProviderCaching)
+            .EnableSensitiveDataLogging(sensitiveDataLoggingEnabled: databaseOptions.EnableSensitiveDataLogging)
+            .EnableThreadSafetyChecks(enableChecks: databaseOptions.EnableThreadSafetyCheck);
+    });
 
 //Httpclient
 services
-	.AddHttpClient(name: "truyenqqne", configureClient: configure =>
-	{
-		configure.DefaultRequestHeaders.Add(name: "Referer", value: "https://truyenqqne.com/");
-		configure.BaseAddress = new(uriString: "https://truyenqqne.com/");
-		configure.Timeout = TimeSpan.FromSeconds(value: 30);
-	});
+    .AddHttpClient(name: "truyenqqne", configureClient: configure =>
+    {
+        configure.DefaultRequestHeaders.Add(name: "Referer", value: "https://truyenqqne.com/");
+        configure.BaseAddress = new(uriString: "https://truyenqqne.com/");
+        configure.Timeout = TimeSpan.FromSeconds(value: 30);
+    });
 
 //controller
 services
-	.AddControllers(configure: option => option.SuppressAsyncSuffixInActionNames = true);
+    .AddControllers(configure: option => option.SuppressAsyncSuffixInActionNames = true);
+
+//RazerPage
+services.AddRazorPages().AddRazorPagesOptions(options =>
+{
+    options.RootDirectory = "/Views";
+});
 
 var app = builder.Build();
 
 //config http/https pipleline
 app
-	.UseHsts()
-	.UseHttpsRedirection()
-	.UseRouting()
-	.UseCors();
+    .UseHsts()
+    .UseHttpsRedirection()
+    .UseStaticFiles()
+    .UseRouting()
+    .UseCors();
 
 app.MapControllers();
+
+app.MapRazorPages();
 
 app.Run();
