9e15828d6a864ab2392f3413b9c03cbbca09666a
Jackpieking <ledinhdangkhoa10a9@gmail.com>
2023-07-13 20:12:23 +0700

    done desgining login and signup form

--- src/Server/MangaManagement/DTO/Incoming/UserDto.cs
+++ src/Server/MangaManagement/DTO/Incoming/UserDto.cs
@@ -10,7 +10,7 @@ public class UserDto
     public string Username { get; set; }
 
     [Required(ErrorMessage = "Please enter the password")]
-    [RegularExpression(pattern: "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{3,}$", ErrorMessage = "Please enter password with at least three characters, at least one uppercase letter, one lowercase letter and one number !!")]
+    [RegularExpression(pattern: "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{3,}$", ErrorMessage = "Please enter password with at least 3 characters, at least 1 uppercase letter, 1 lowercase letter and one number !!")]
     public string Password { get; set; }
 
     [RegularExpression(pattern: "^[a-zA-Z_ÀÁÂÃÈÉÊẾÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêếìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶẸẺẼỀỀỂưăạảấầẩẫậắằẳẵặẹẻẽềềểỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ\\ ]{2,}$", ErrorMessage = "Please enter user fullname with at least 2 letter !!")]
